051f9c45f2b12feae9cf80d375573740c3a72c4c
insulee <insulee@users.noreply.github.com>
2026-02-15 13:04:32 +0900

    Quartz sync: Feb 15, 2026, 1:04 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -36,7 +36,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({ folderClickBehavior: "collapse", folderDefaultState: "collapsed" }),
   ],
   right: [
     Component.Graph(),
@@ -60,7 +60,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({ folderClickBehavior: "collapse", folderDefaultState: "collapsed" }),
   ],
   right: [],
 }
